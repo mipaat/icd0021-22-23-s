78d241a46c98bc617232d95ebdc145b130501645
mipaat <mikkel.paat@gmail.com>
2023-05-31 01:05:07 +0300

    Localize shared partial views (except error page)

--- VideoArchiver/WebApp/ViewModels/LinkSubmissionViewModel.cs
+++ VideoArchiver/WebApp/ViewModels/LinkSubmissionViewModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 #pragma warning disable CS1591
 namespace WebApp.ViewModels;
 
 public class LinkSubmissionViewModel
 {
+    [Display(Name = nameof(Link), Prompt = nameof(Link) + "Prompt", ResourceType = typeof(App.Resources.WebApp.ViewModels.LinkSubmissionViewModel))]
     public string Link { get; set; } = default!;
+    [Display(Name = nameof(SubmitPlaylist), ResourceType = typeof(App.Resources.WebApp.ViewModels.LinkSubmissionViewModel))]
     public bool SubmitPlaylist { get; set; }
 }
