47f1d5f0a33a1cb4b7ca67f76b2ece04592ecb2b
mipaat <mikkel.paat@gmail.com>
2023-06-03 14:01:04 +0300

    Finish MVC integration test?

--- VideoArchiver/Tests/Integration/MVC/MvcHappyFlowTests.cs
+++ VideoArchiver/Tests/Integration/MVC/MvcHappyFlowTests.cs
@@ -10,11 +10,9 @@ namespace Tests.Integration.MVC;
 public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
     private readonly HttpClient _client;
-    private readonly CustomWebApplicationFactory<Program> _factory;
 
     public MvcHappyFlowTests(CustomWebApplicationFactory<Program> factory)
     {
-        _factory = factory;
         _client = factory.CreateClient(new WebApplicationFactoryClientOptions
         {
             AllowAutoRedirect = false,
@@ -24,6 +22,64 @@ public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Progr
     [Fact]
     public async Task TestRegisterApproveSubmitView()
     {
+        const string testUserName = "testuser";
+        const string testUserPassword = "testpassword";
+        await RegisterApproveAndAddAdminTestUser(testUserName, testUserPassword);
+        await LoginAddVideoSearchVideoWatchVideoAsAdminUser(testUserName, testUserPassword);
+    }
+
+    private async Task LoginAddVideoSearchVideoWatchVideoAsAdminUser(string userName, string password)
+    {
+        var loginResponse = await LogIn(userName, password);
+        Assert.Equal(HttpStatusCode.Found, loginResponse.StatusCode);
+        var loginRedirectResponse = await _client.GetAsync(loginResponse.Headers.Location);
+        Assert.Equal(HttpStatusCode.Found, loginRedirectResponse.StatusCode);
+        var selectAuthorRedirectResponse = await _client.GetAsync(loginRedirectResponse.Headers.Location);
+        selectAuthorRedirectResponse.EnsureSuccessStatusCode();
+
+        var homePageDocument = await selectAuthorRedirectResponse.GetDocumentAsync();
+        var linkSubmitForm = homePageDocument.QuerySelector("#link-submit-form") as IHtmlFormElement
+                             ?? throw new Exception("Link submission form not found");
+        var linkSubmitInput = linkSubmitForm
+                                  .QuerySelectorAll("input")
+                                  .FirstOrDefault(e => e is IHtmlInputElement { Name: "Link" }) as IHtmlInputElement
+                              ?? throw new Exception("Link submission input not found");
+        linkSubmitInput.Value = "https://www.youtube.com/watch?v=ojxw4K_IWz8";
+        var linkSubmitResponse = await SubmitForm(linkSubmitForm);
+        Assert.Equal(HttpStatusCode.Found, linkSubmitResponse.StatusCode);
+        var linkSubmitRedirectResponse = await _client.GetAsync(linkSubmitResponse.Headers.Location);
+        linkSubmitRedirectResponse.EnsureSuccessStatusCode();
+        var linkSubmitRedirectDocument = await linkSubmitRedirectResponse.GetDocumentAsync();
+        Assert.Contains(linkSubmitRedirectDocument.QuerySelectorAll("*")
+, e => e.TextContent.ToLower().Contains("successfully processed"));
+
+        var videoSearchResponse = await _client.GetAsync("/Video/Search/");
+        var videoSearchDocument = await videoSearchResponse.GetDocumentAsync();
+        var videoSearchForm = videoSearchDocument.QuerySelector("#video-search-form") as IHtmlFormElement
+                              ?? throw new Exception("Video search form not found");
+        var nameQueryInput = videoSearchForm.QuerySelectorAll("input")
+                                 .FirstOrDefault(e => e is IHtmlInputElement { Name: "NameQuery" }) as IHtmlInputElement
+                             ?? throw new Exception("Video search name query form element not found");
+        const string videoTitle = "happy llama sad llama";
+        nameQueryInput.Value = videoTitle;
+        var videoSearchWithQueryResponse = await SubmitForm(videoSearchForm, useQueryString: true);
+        videoSearchWithQueryResponse.EnsureSuccessStatusCode();
+
+        var videoSearchWithQueryDocument = await videoSearchResponse.GetDocumentAsync();
+        var videoWatchLink = videoSearchWithQueryDocument.QuerySelectorAll("a")
+            .FirstOrDefault(e => e.TextContent == videoTitle) as IHtmlAnchorElement
+            ?? throw new Exception("Video watch link not found");
+        var videoWatchPageResponse = await _client.GetAsync(videoWatchLink.Href);
+        videoWatchPageResponse.EnsureSuccessStatusCode();
+        var videoWatchPageDocument = await videoWatchPageResponse.GetDocumentAsync();
+        Assert.Contains(videoWatchPageDocument.QuerySelectorAll("*"), e => e.TextContent == videoTitle);
+        Assert.NotNull(videoWatchPageDocument.QuerySelector("video"));
+
+        await LogOut(videoWatchPageDocument);
+    }
+
+    private async Task RegisterApproveAndAddAdminTestUser(string testUserName, string testUserPassword)
+    {
         var homePageResponse = await _client.GetAsync("/");
         homePageResponse.EnsureSuccessStatusCode();
         var homePageContent = await homePageResponse.GetDocumentAsync();
@@ -32,7 +88,7 @@ public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Progr
                            ?? throw new Exception("Register link not found");
         var goToRegisterPageResponse = await _client.GetAsync(registerLink.Href);
         goToRegisterPageResponse.EnsureSuccessStatusCode();
-        var registerResponse = await Register("testuser", "testpassword", goToRegisterPageResponse);
+        var registerResponse = await Register(testUserName, testUserPassword, goToRegisterPageResponse);
         Assert.Equal(HttpStatusCode.Found, registerResponse.StatusCode);
         var registerRedirectResponse = await _client.GetAsync(registerResponse.Headers.Location);
         registerRedirectResponse.EnsureSuccessStatusCode();
@@ -53,30 +109,30 @@ public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Progr
         var userManagementIndexResponse = await _client.GetAsync($"/Admin/UserManagement/Index/");
         userManagementIndexResponse.EnsureSuccessStatusCode();
         var userManagementIndexDocument = await userManagementIndexResponse.GetDocumentAsync();
-        var userManagementDiv = userManagementIndexDocument.QuerySelector("#manage-testuser")
+        var userManagementDiv = userManagementIndexDocument.QuerySelector($"#manage-{testUserName}")
                                 ?? throw new Exception("User management div not found");
-        var userApprovalForm = userManagementDiv.QuerySelector("#approve-testuser") as IHtmlFormElement
-            ?? throw new Exception("User approval form not found");
+        var userApprovalForm = userManagementDiv.QuerySelector($"#approve-{testUserName}") as IHtmlFormElement
+                               ?? throw new Exception("User approval form not found");
         var approveUserResponse = await SubmitForm(userApprovalForm);
         Assert.Equal(HttpStatusCode.Found, approveUserResponse.StatusCode);
         var approveUserRedirectResponse = await _client.GetAsync(approveUserResponse.Headers.Location);
         approveUserRedirectResponse.EnsureSuccessStatusCode();
 
         userManagementIndexDocument = await approveUserRedirectResponse.GetDocumentAsync();
-        userManagementDiv = userManagementIndexDocument.QuerySelector("#manage-testuser")
+        userManagementDiv = userManagementIndexDocument.QuerySelector($"#manage-{testUserName}")
                             ?? throw new Exception("User management div not found");
-        Assert.Null(userManagementDiv.QuerySelector("#approve-testuser"));
+        Assert.Null(userManagementDiv.QuerySelector($"#approve-{testUserName}"));
         var manageRolesLink = userManagementDiv
-            .QuerySelectorAll("a")
-            .FirstOrDefault(e => e.TextContent.ToLower().Contains("roles"))
-                              as IHtmlAnchorElement
-            ?? throw new Exception("Role management link not found");
+                                      .QuerySelectorAll("a")
+                                      .FirstOrDefault(e => e.TextContent.ToLower().Contains("roles"))
+                                  as IHtmlAnchorElement
+                              ?? throw new Exception("Role management link not found");
 
         var manageRolesGetResponse = await _client.GetAsync(manageRolesLink.Href);
         manageRolesGetResponse.EnsureSuccessStatusCode();
         var manageRolesDocument = await manageRolesGetResponse.GetDocumentAsync();
         var addAdminForm = manageRolesDocument.QuerySelector("#add-Admin-form") as IHtmlFormElement
-            ?? throw new Exception("Add admin role form not found");
+                           ?? throw new Exception("Add admin role form not found");
         var addAdminRoleResponse = await SubmitForm(addAdminForm);
         Assert.Equal(HttpStatusCode.Found, addAdminRoleResponse.StatusCode);
         var addAdminRoleRedirect = await _client.GetAsync(addAdminRoleResponse.Headers.Location);
@@ -103,7 +159,7 @@ public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Progr
     private async Task<HttpResponseMessage> LogOut(IHtmlDocument document)
     {
         var logoutForm = document.QuerySelector("#logout-form") as IHtmlFormElement
-            ?? throw new Exception("Logout form not found");
+                         ?? throw new Exception("Logout form not found");
         var logOutResponse = await SubmitForm(logoutForm);
         Assert.Equal(HttpStatusCode.Found, logOutResponse.StatusCode);
         return await _client.GetAsync(logOutResponse.Headers.Location);
@@ -127,7 +183,7 @@ public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Progr
     }
 
     private async Task<HttpResponseMessage> SubmitForm(IHtmlFormElement formElement,
-        IDictionary<string, string>? data = null, string? verificationToken = null)
+        IDictionary<string, string>? data = null, string? verificationToken = null, bool? useQueryString = null)
     {
         data ??= new Dictionary<string, string>();
         foreach (var formControl in formElement.Elements)
@@ -138,13 +194,15 @@ public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Progr
                 var value = HttpUtility.UrlDecode(inputElement.Value ?? string.Empty);
                 data[inputElement.Name] = value;
             }
-            else if (formControl is IHtmlTextAreaElement textAreaElement && !string.IsNullOrEmpty(textAreaElement.Name) &&
+            else if (formControl is IHtmlTextAreaElement textAreaElement &&
+                     !string.IsNullOrEmpty(textAreaElement.Name) &&
                      !data.Keys.Any(k => string.Equals(k, textAreaElement.Name, StringComparison.OrdinalIgnoreCase)))
             {
                 var value = HttpUtility.UrlDecode(textAreaElement.Value ?? string.Empty);
                 data[textAreaElement.Name] = value;
             }
         }
+
         verificationToken ??= (formElement["__RequestVerificationToken"] as IHtmlInputElement)?.Value;
         if (verificationToken != null)
         {
@@ -152,7 +210,26 @@ public class MvcHappyFlowTests : IClassFixture<CustomWebApplicationFactory<Progr
         }
 
         var request = new HttpRequestMessage(new HttpMethod(formElement.Method), formElement.Action);
-        request.Content = new FormUrlEncodedContent(data);
+
+        useQueryString ??= formElement.Method.ToLower() == "get";
+        if (useQueryString.Value)
+        {
+            var uriBuilder = new UriBuilder(request.RequestUri ?? throw new Exception("Request URI was null"));
+            var queryString = string.Join("&", data.Select(kvp => $"{Uri.EscapeDataString(kvp.Key)}={Uri.EscapeDataString(kvp.Value)}"));
+
+            if (!string.IsNullOrEmpty(uriBuilder.Query))
+            {
+                queryString = uriBuilder.Query + "&" + queryString;
+            }
+
+            uriBuilder.Query = queryString;
+            request.RequestUri = uriBuilder.Uri;
+        }
+        else
+        {
+            request.Content = new FormUrlEncodedContent(data);
+        }
+
         return await _client.SendAsync(request);
     }
 }
